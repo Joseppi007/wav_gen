975a2ea0cfa8c6fd639afea52d94f25813c5f44a
Joseppi007 <joeynovack314@gmail.com>
2024-03-19 17:52:21 -0400

    Fixed the header

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,12 @@ fn scale_time_sin (time_in: u128, frequency_a: f64, frequency_b: f64, meta_frequ
     frequency_mid * time + frequency_diff * (1.0/(std::f64::consts::TAU*meta_frequency)*(meta_frequency*std::f64::consts::TAU*time).sin())
 }
 
-fn write_sample (amplitude: f64) -> () {
+fn sample_data (amplitude: f64) -> [u8; 2] {
     let mut a: f64 = amplitude * 65535.0 - 32768.0;
     if a > 32767.0 { a = 32767.0; }
     if a < -32768.0   { a = -32768.0;   }
     let double_byte: i16 = a as i16;
-    let _ = std::io::stdout().write(&double_byte.to_le_bytes());
+    double_byte.to_le_bytes()
 }
 
 #[derive(Copy, Clone)]
@@ -135,7 +135,8 @@ impl MetaData {
 }
 
 fn print_wave( file: File ) -> () {
-    let _ = std::io::stdout().write(&[82, 73, 70, 70, 62, 250, 0, 0, 87, 65, 86, 69, 102, 109, 116, 32, 16, 0, 0, 0, 1, 0, CHANNEL_COUNT, 0, 128, 62, 0, 0, 0, 125, 0, 0, 2, 0, 16, 0, 76, 73, 83, 84, 26, 0, 0, 0, 73, 78, 70, 79, 73, 83, 70, 84, 14, 0, 0, 0, 76, 97, 118, 102, 54, 48, 46, 49, 54, 46, 49, 48, 48, 0, 100, 97, 116, 97, 248, 249]);
+    // let _ = std::io::stdout().write(&[82, 73, 70, 70, 62, 250, 0, 0, 87, 65, 86, 69, 102, 109, 116, 32, 16, 0, 0, 0, 1, 0, CHANNEL_COUNT, 0, 128, 62, 0, 0, 0, 125, 0, 0, 2, 0, 16, 0, 76, 73, 83, 84, 26, 0, 0, 0, 73, 78, 70, 79, 73, 83, 70, 84, 14, 0, 0, 0, 76, 97, 118, 102, 54, 48, 46, 49, 54, 46, 49, 48, 48, 0, 100, 97, 116, 97, 248, 249]);
+    let mut data_buffer: Vec<u8> = Vec::<u8>::new();
     
     let lines = std::io::BufReader::new(file).lines();
 
@@ -215,8 +216,14 @@ fn print_wave( file: File ) -> () {
 	    if current_time > note_start_time_accumulator + note.duration { continue; }
 	    audio_accumulator += note.audio_at(current_time);
 	}
-	write_sample( audio_accumulator );
+	data_buffer.extend_from_slice(&sample_data(audio_accumulator));
     }
+
+    let _ = std::io::stdout().write(&[82, 73, 70, 70]);
+    let _ = std::io::stdout().write(&(data_buffer.len() as u32 + 68).to_le_bytes());
+    let _ = std::io::stdout().write(&[87, 65, 86, 69, 102, 109, 116, 32, 16, 0, 0, 0, 1, 0, CHANNEL_COUNT, 0, 128, 62, 0, 0, 0, 125, 0, 0, 2, 0, 16, 0, 76, 73, 83, 84, 26, 0, 0, 0, 73, 78, 70, 79, 73, 83, 70, 84, 14, 0, 0, 0, 76, 97, 118, 102, 54, 48, 46, 49, 54, 46, 49, 48, 48, 0, 100, 97, 116, 97]);
+    let _ = std::io::stdout().write(&(data_buffer.len() as u32).to_le_bytes());
+    let _ = std::io::stdout().write(&data_buffer);
 }
 
 fn main() {
